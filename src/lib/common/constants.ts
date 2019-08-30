71e7fbb8e9083f14850c4657e6469a236efcff03
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-08-30 11:51:45 +0200

    style(env variables): rename Env variables

--- src/lib/common/constants.ts
+++ src/lib/common/constants.ts
@@ -1,8 +1,8 @@
 import { IBathingspot } from './interfaces';
-// export const API_DOMAIN =
-//   'http://flsshygn-dev.eu-central-1.elasticbeanstalk.com';
+export const API_DOMAIN =
+  'http://flsshygn-dev.eu-central-1.elasticbeanstalk.com';
 
-export const API_DOMAIN = '';
+// export const API_DOMAIN = '';
 export const DEFAULT_SPOT_ID = 999999999;
 
 export const DEFAULT_SPOT: IBathingspot = {
