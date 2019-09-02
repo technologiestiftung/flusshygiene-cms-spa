8ae9c881d9f32f9d704c2a3955d9b17ce84bb8e8
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-09-02 09:13:32 +0200

    test(update): Update tests for CardTile

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
-import { Card } from '../components/spot/Card';
+import { CardTile } from '../components/spot/CardTile';
 import { MemoryRouter } from 'react-router';
 
 storiesOf('Card', module)
@@ -9,7 +9,7 @@ storiesOf('Card', module)
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
   .add('default', () => (
-    <Card
+    <CardTile
       title={'Sweetwater'}
       water={'Sweet'}
       id={1}
