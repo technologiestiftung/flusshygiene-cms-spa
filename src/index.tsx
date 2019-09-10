0c240ac500c0b139119467075199d88311c1c91e
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-09-10 14:23:37 +0200

    fix(auth provider): reanable auth provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './assets/styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { Auth0Provider } from './react-auth0-wrapper';
-import { Auth0Provider } from './react-auth0-wrapper-offline';
+import { Auth0Provider } from './react-auth0-wrapper';
+// import { Auth0Provider } from './react-auth0-wrapper-offline';
 
 // import config from './auth_config.json';
 import { Provider } from 'react-redux';
