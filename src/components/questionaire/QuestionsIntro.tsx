ca50d55125625038777313d754e2b3aa1dd93839
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-10-22 13:00:14 +0200

    remove unused import

--- src/components/questionaire/QuestionsIntro.tsx
+++ src/components/questionaire/QuestionsIntro.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { QIntro, QIntroNew } from './QIntro';
+import { QIntroNew } from './QIntro';
 //import { QuestionsProvider } from '../../contexts/questionaire';
 
 export const QuestionsIntro: React.FC = () => {
